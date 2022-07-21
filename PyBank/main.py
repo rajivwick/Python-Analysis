280b53f53e97448dd77015cdff217059555285b1
rajiv_wick@iinet.net.au <rajiv_wick@iinet.net.au>
2022-07-21 23:22:42 +0800

    Added notes to both scripts

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,17 @@
+#Dependencies
 import os
 import csv
 
+#Import CSV file from Resource Folder
 Budget_csv = os.path.join('Resources', 'budget_data.csv')
-budget_dict = {}
-delta_pnl = {}
-results = []
 
+#Global dictionaries and lists
+budget_dict = {}    # Created to store the Budget data as a Dictionary.
+delta_pnl = {}      # Created to store a differences of PNL from month to month as a Dictionary.
+results = []        # Created to store the final results in to a list. This list is then printed to terminal and txt file.
+
+#Function: Imports the Budget dictionary, calculates the sum of all PNL values within the data set and exports the value.
+#Operation: Iterates through the dictionary by the key (Month value) and adds the value (PNL value) to the 'sumpnl' variable.
 def sum_pnl_func(budget_dict):
     sumpnl = 0    
     for key, value in budget_dict.items():
@@ -13,29 +19,41 @@ def sum_pnl_func(budget_dict):
 
     return sumpnl
 
+#Function: Imports the Budget dictionary and total months variable, calculates the difference of PNL between each month and returns the average.
 def delta_pnl_func(budget_dict, totMonths):
-    start = 1
-    total = 0
+    # Declare local variables
+    start = 1   #This variable is to define the start of this calculation and is needed to allow the function to enter the first condition.
+    total = 0   #This variable is used to store the total value within the created dictionary, delta_pnl. Initialized to start at 0.
+    delta = 0   #This variable holds the value of the difference in PNL between the current and previous month.
+    
+    #Iterate through the budget dictionary
     for key, value in budget_dict.items():
         
-        x = float(value)
-        if start == 1:
-            delta = 0
+        
+        PNL = float(value)
+
+        # This condition is set to allow the calculation to have the first difference in PNL be set to 0, as the first month has no comparison to find a difference against.
+        if start == 1: 
             delta_pnl[key] = delta
-            start = 0
+            # As we have now concluded with the first key, we can set the start variable to 0.
+            start = 0            
+        
         else:
-            delta = (x - y)
+            delta = (PNL - Last_PNL)
+            #Store this value into the delta_pnl dictionary, with the key (Month) being the same as the current key (Month) and the value to be the delta calculated above.
             delta_pnl[key] = delta
-        y = x
-       
+        #Set the Last_PNL variable to equal the current PNL before going into the next key(Month)
+        Last_PNL = PNL
+    #Iterate through delta_pnl    
     for key, value in delta_pnl.items():
         
         total = total + int(value)
-    
+    #Calculate the average 
     average = round(total/(totMonths-1),2)
     
     return average    
 
+#Function: Import the delta_pnl dictionary and search through to determine the max profit difference store as a value, as well as the Month and returns both of this values.  
 def max_profit(delta_pnl):
     maxprofit = 0
 
@@ -46,6 +64,7 @@ def max_profit(delta_pnl):
             maxmonth = key
     return maxmonth, maxprofit
 
+#Function: Import the delta_pnl dictionary and search through to determine the min profit store as a value, as well as the Month and returns both of this values.  
 def min_profit(delta_pnl):
     minprofit = 0
 
@@ -56,6 +75,8 @@ def min_profit(delta_pnl):
             minmonth = key
     return minmonth, minprofit
 
+#Function: Imports all the necessary data to put into the results list which is then returned. 
+#Function: Single point of result construction - If any changes are required for the output, edit here and results will print in terminal and on the txt file output.
 def output(totMonths, sumpnl, average, max, min):
     
     results.append("Financial Analysis")
@@ -65,28 +86,36 @@ def output(totMonths, sumpnl, average, max, min):
     results.append(f"Average Change: ${str(average)}")
     results.append(f"Greatest Increase in Profits: {str(max[0])} ({str(max[1])})")
     results.append(f"Greatest Decrease in Profits: {str(min[0])} ({str(min[1])})")
-        
+
+       
 with open(Budget_csv) as csvfile:
 
     Budget = csv.reader(csvfile, delimiter=',')
+    #Store title row as header list
     header = next(Budget)
-    
+    #Store the remaining csv into a dictionary, column 0 to denote our key(Months) and column 1 to denote our value(PNL)
     budget_dict = {rows[0]:rows[1] for rows in Budget}
 
+    #Store the value of the length of the dictionary - number of rows of data
     totMonths = len(list(budget_dict))
+    
     sumpnl = int(sum_pnl_func(budget_dict))
+
     average = delta_pnl_func(budget_dict, totMonths)
+    #Store the values returned from the function as a list. [month, value]
     max = max_profit(delta_pnl)
+   #Store the values returned from the function as a list. [month, value]
     min = min_profit(delta_pnl)
+    #Creating the results list
     output(totMonths,sumpnl, average,max,min)
-        
+    #Outputting the results to terminal   
     for line in results:
         print(line)
         
 output_path = os.path.join('Analysis', 'Results.txt')
 
 with open(output_path, "w") as datafile:
-    
+    #Outputting the results to txt file  
     for line in results:
         datafile.write(line)
         datafile.write('\n')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,41 +1,47 @@
+#Dependencies
 import os
 import csv
 
+#Import CSV file from Resource Folder
 election_csv = os.path.join('Resources', 'election_data.csv')
+
+#Global dictionaries and lists
 election_list = []
 results = []
 data_dict = {}      
 output_results = {}
 
-
+#Function: (Duplicate) Created to print a new value against the key (Candidate) in the dictionary when a candidate is found duplicate times in the data set. 
 def update_new_total_func(value, totV, last):
     newtotal = int(value) + totV
     data_dict[last] = newtotal
     duplicate  = "y" 
     return duplicate  
-   
+
+#Function: Iterate through the main data list, and then store the count of votes per candidate into a dictionary    
 def calc_percentage_func(election_list):
     
-    last = "nil"
-    totV = 0
-    start = 0
-    duplicate = "n"
-    addTot = 0
+    last = "nil"    #Initialized last, variable that is intended to hold the previous candidate name
+    totV = 0        #Initialized to 0 - counter used to tally votes
+    start = 0       #Initialized to 0 to allow for the first iteration not to trigger the first IF statement. 
+    duplicate = "n" #Initialized to No - referencing we are not storing a duplicate key when updating data_dict
+    addTot = 0      #Initialized to 0 - counter to tally number of iterations - used to determine the end of dataset.
     size = len(election_list)
     
     
     for row in election_list:
 
-
+        #This candidate is met when we have reached a row where the candidate isn't the same as the previous row.
         if (row[2] != last and start == 1):
         
-            addTot += 1
-           
+            addTot += 1  #count for every iteration.
+            #Search the data_dict for whether the candidate being tallied is already within its dictionary 
             for key,value in data_dict.items():
                 
-                if key == last:          
+                if key == last:
+                    #If the candidate already has a value against it's key, then enter the duplicate function to update the value against that key and return the duplicate indicator.
                     duplicate = update_new_total_func(value, totV, last)
-                    
+            #If the candidate isnt currently in the dictionary then add it and store the new value as the totV tallied.        
             if duplicate == "n":   
                 data_dict[last] = totV
                 totV = 1
@@ -43,36 +49,40 @@ def calc_percentage_func(election_list):
             else:  
                 totV = 1
                 last = row[2] 
-                #print("reset total happened after data_dict was adjusted due to duplicate")
+                
         else:
-            addTot += 1
+            addTot += 1 #count for every iteration.
             totV += 1
             last = row[2]
-            start = 1
-           
+            start = 1   #Set start to 1 after the first iteration to allow for the first condition to be met when applicable. 
+            #We need to make sure the data is saved once we reach the end of our list, we compare the iteration tally against the length of the list. 
             if size == addTot:
                 for key,value in data_dict.items():
                     if key == last:
                         duplicate = update_new_total_func( value, totV, last)            
             duplicate = "n"
 
-    
+    #Created a copy of our data_dict to manipulate for output results.
     for key, value in data_dict.items():
         output_results[key] = value
     
+    #Calculate average and store it into our data_dict
     for key,value in data_dict.items():
         average = round((float(value) / size)*100,3)
         data_dict[key] = average
-       
+
+    #Calculate the average and store it as a string in output_results   
     for key,value in output_results.items():
         average = str(round((float(value) / size)*100,3)) + "%"+ " " + "(" + str(value) + ")"
         output_results[key] = average
-            
+
+#Function: Finds the max value inside the data_dict, then stores and returns the key of this value.            
 def winner_func(data_dict):
 
     win_candidate = max(data_dict, key=data_dict.get)
     return win_candidate
 
+#Function: Creates results list - which is then used to print to terminal and txt file. Any future changes to output text, change within here. 
 def output_func(output_results, winner):
 
     results.append("Election Results")
@@ -87,21 +97,26 @@ def output_func(output_results, winner):
     
     
 with open(election_csv) as csvfile:
+    
     election = csv.reader(csvfile, delimiter=',')
-    
+    #store titles as header variable
     header = next(election)
+    #store the csv data as a list
     election_list = list(election)
+    #calculate the percentage of votes each candidate received 
     calc_percentage_func(election_list)
+    #store the winning candidates name 
     winner = winner_func(data_dict)
+    #create results list
     output_func(output_results, winner)
-    
+    #output results on terminal
     for line in results:
         print(line)
-
+  
 output_path = os.path.join('Analysis', 'Results.txt')
 
 with open(output_path, "w") as datafile:
-    
+    #output results to txt file
     for line in results:
         datafile.write(line)
         datafile.write('\n')
